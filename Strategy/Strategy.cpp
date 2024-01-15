119010c811dacce591dd941b6214180271247902
Slavik <ivantsiv@yahoo.com>
2024-01-15 22:23:31 +0100

    Added Strategy design pattern implementation

---
+++ Strategy/Strategy.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <vector>
+
+enum class OutputFormat
+{
+    Markdown,
+    Html
+};
+
+struct ListStrategy
+{
+    virtual ~ListStrategy() = default;
+    virtual void add_list_item(std::ostringstream& oss, const std::string& item) {};
+    virtual void start(std::ostringstream& oss) {};
+    virtual void end(std::ostringstream& oss) {};
+};
+
+struct MarkdownListStrategy : ListStrategy
+{
+    void add_list_item(std::ostringstream& oss, const std::string& item) override
+    {
+        oss << " * " << item << std::endl;
+    }
+};
+
+struct HtmlListStrategy : ListStrategy
+{
+    void start(std::ostringstream& oss) override
+    {
+        oss << "<ul>" << std::endl;
+    }
+
+    void end(std::ostringstream& oss) override
+    {
+        oss << "</ul>" << std::endl;
+    }
+
+    void add_list_item(std::ostringstream& oss, const std::string& item) override
+    {
+        oss << "<li>" << item << "</li>" << std::endl;
+    }
+};
+
+struct TextProcessor
+{
+    void clear()
+    {
+        oss.str("");
+        oss.clear();
+    }
+
+    void append_list(const std::vector<std::string> items)
+    {
+        list_strategy->start(oss);
+        for (auto& item : items)
+            list_strategy->add_list_item(oss, item);
+        list_strategy->end(oss);
+    }
+
+    void set_output_format(const OutputFormat format)
+    {
+        switch (format)
+        {
+        case OutputFormat::Markdown:
+            list_strategy = std::make_unique<MarkdownListStrategy>();
+            break;
+        case OutputFormat::Html:
+            list_strategy = std::make_unique<HtmlListStrategy>();
+            break;
+        default:
+            std::cout << "Unsupported strategy." << std::endl;
+        }
+    }
+
+    std::string str() const { return oss.str(); }
+
+private:
+    std::ostringstream oss;
+    std::unique_ptr<ListStrategy> list_strategy;
+};
+
+int main()
+{
+    // Markdown format
+    TextProcessor tp;
+    tp.set_output_format(OutputFormat::Markdown);
+    tp.append_list({ "foo", "bar", "baz" });
+    std::cout << tp.str() << std::endl;
+
+    // HTML format
+    tp.clear();
+    tp.set_output_format(OutputFormat::Html);
+    tp.append_list({ "foo", "bar", "baz" });
+    std::cout << tp.str() << std::endl;
+
+    return 0;
+}
