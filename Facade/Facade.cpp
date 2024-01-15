4fcbcc868a558b9344234f2400a86f12470d5f23
Slavik <ivantsiv@yahoo.com>
2024-01-15 21:37:29 +0100

    Added Facade design pattern

---
+++ Facade/Facade.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class SubsystemA 
+{
+public:
+    void operationA() const 
+    {
+        std::cout << "Subsystem A operation" << std::endl;
+    }
+};
+
+class SubsystemB 
+{
+public:
+    void operationB() const 
+    {
+        std::cout << "Subsystem B operation" << std::endl;
+    }
+};
+
+class Facade 
+{
+public:
+    Facade() : subsystemA(), subsystemB() {}
+
+    void operation() const 
+    {
+        std::cout << "Facade operation:" << std::endl;
+        subsystemA.operationA();
+        subsystemB.operationB();
+    }
+
+private:
+    SubsystemA subsystemA;
+    SubsystemB subsystemB;
+};
+
+int main() 
+{
+    Facade facade;
+    facade.operation();
+
+    return 0;
+}
