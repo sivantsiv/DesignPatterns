f21b1cb5332a9d922f14ee7b74a3173249f20e09
Slavik <ivantsiv@yahoo.com>
2024-02-01 22:20:57 +0100

    Added Observer design pattern implementation

---
+++ Observer/headers.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+#include <cmath>
+#include <map>
+#include <ostream>
+#include <cstdint>
+#include <functional>
+#include <typeindex>
+#include <any>
+#include <boost/signals2.hpp>
+
+using namespace boost;
+using namespace boost::signals2;
