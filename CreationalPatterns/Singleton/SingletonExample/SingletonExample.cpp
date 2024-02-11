22c4673b370407c1cd1f062bb8e37e8b6f300302
Slavik <ivantsiv@yahoo.com>
2024-02-11 12:40:09 +0100

    Added Singleton design pattern example

---
+++ CreationalPatterns/Singleton/SingletonExample/SingletonExample.cpp
@@ -0,0 +1,38 @@
+﻿#include <functional>
+#include "gtest/gtest.h"
+
+class SingletonTester
+{
+public:
+    template <typename T>
+    bool is_singleton(std::function<T* ()> factory)
+    {
+        T* singletoneFirst = factory();
+        T* singletoneSecond = factory();
+        return singletoneFirst == singletoneSecond;
+    }
+};
+
+namespace
+{
+    class Evaluate : public testing::Test
+    {
+    };
+
+    TEST_F(Evaluate, SimpleTest)
+    {
+        SingletonTester tester;
+        ASSERT_TRUE(tester.is_singleton<SingletonTester>(
+            [&]() -> SingletonTester* { return &tester; }))
+            << "Expected a singleton in this case; provided lambda is yielding the same object over and over.";
+        ASSERT_FALSE(tester.is_singleton<SingletonTester>([]() { return new SingletonTester{}; }));
+    }
+} // namespace
+
+
+int main(int ac, char* av[])
+{
+    testing::InitGoogleTest(&ac, av);
+
+    return RUN_ALL_TESTS();
+}
